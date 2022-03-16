1a6229920fab9f9f1dce02c29574c380bd8279f9
Ariful Alam <swazan.arif@gmail.com>
2022-03-17 01:43:00 +0600

    Enable sitemap

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,6 +36,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        sitemap: {
+          changefreq: 'weekly',
+          priority: 0.5,
+        },
       }),
     ],
   ],
