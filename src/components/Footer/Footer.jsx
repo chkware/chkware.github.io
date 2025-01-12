4365524030522611ddd46bcbb7b4637243917880
Tawshif Ahsan Khan <anikphp@gmail.com>
2025-01-12 18:21:52 +0600

    feat: system structure for theme development

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+// src/components/Footer/Footer.jsx
+import React from 'react';
+import styles from './Footer.module.scss';
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>&copy; 2025 My Website. All rights reserved.</p>
+    </footer>
+  );
+}
